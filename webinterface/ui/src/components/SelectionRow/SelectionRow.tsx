3d1c11c0465e3165b7530940d39dd0ec6b1aa485
clzander <cle.zander@gmail.com>
2023-11-22 12:28:40 +0100

    backend is no longer requested in an infinite loop

--- webinterface/ui/src/components/SelectionRow/SelectionRow.tsx
+++ webinterface/ui/src/components/SelectionRow/SelectionRow.tsx
@@ -1,31 +1,37 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import { NewsData } from '../../App';
+import './SelectionRow.css'
 
 interface SelectionRowProps {
-    onUpdate: (timestamp: string) => void;
+    onUpdate: Dispatch<SetStateAction<NewsData[]>>;
 }
 
 export default function SelectionRow({ onUpdate }: SelectionRowProps) {
     const [timestamp, setTimestamp] = useState<string>('this_week');
-
-    const fetchData = useCallback((timestamp: string) => {
-        onUpdate(timestamp);
-    }, [onUpdate]);
+    const [clicked, setClicked] = useState<boolean>(false);
 
     useEffect(() => {
-        fetchData(timestamp);
-    }, [timestamp, fetchData]);
+        console.log("Fetched")
+        fetch("http://127.0.0.1:5001/api/" + timestamp)
+            .then((response) => response.json())
+            .then((data) => onUpdate(data))
+            .catch((error) => console.error(error));
+    }, [timestamp, clicked, onUpdate]);
 
     return (
         <div className='selection-row-box'>
-            <button onClick={() => fetchData(timestamp)}>Update</button>
+            <div className='selection-row-text-box'>
+                <h2>Visualizing Data from</h2>
 
-            <select value={timestamp} onChange={e => setTimestamp(e.target.value)}>
-                <option value={'today'}>Today</option>
-                <option value={'this_week'}>This Week</option>
-                <option value={'this_month'}>This Month</option>
-                <option value={'this_year'}>This Year</option>
-                <option value={'custom'}>Custom Range</option>
-            </select>
+                <select value={timestamp} onChange={e => setTimestamp(e.target.value)}>
+                    <option value={'today'}>Today</option>
+                    <option value={'this_week'}>This Week</option>
+                    <option value={'this_month'}>This Month</option>
+                    <option value={'this_year'}>This Year</option>
+                    <option value={'custom'}>Custom Range</option>
+                </select>
+            </div>
+            <button onClick={() => clicked ? setClicked(false) : setClicked(true)}>↺</button>
         </div>
     );
 }
