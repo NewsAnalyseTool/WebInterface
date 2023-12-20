492f21096cec93ae68515965a514750e390c762f
clzander <cle.zander@gmail.com>
2023-12-20 11:08:04 +0100

    frontend requests general endpoint from v2 api version

--- ui/src/components/SelectionRow/SelectionRow.tsx
+++ ui/src/components/SelectionRow/SelectionRow.tsx
@@ -9,11 +9,13 @@ interface SelectionRowProps {
 
 export default function SelectionRow({ onUpdate }: SelectionRowProps) {
     const [timestamp, setTimestamp] = useState<string>('this_week');
+    const [startDate, setStartDate] = useState<string>();
+    const [endDate, setEndDate] = useState<string>();
     const [clicked, setClicked] = useState<boolean>(false);
 
     useEffect(() => {
         console.log("Fetched")
-        fetch(`http://${config.apiIP}:${config.apiPort}/api`)
+        fetch(`http://${config.apiIP}:${config.apiPort}/api/v2/general?startDate${startDate}&endDate${endDate}`)
             .then((response) => response.json())
             .then((data) => onUpdate(data))
             .catch((error) => console.error(error));
@@ -21,6 +23,25 @@ export default function SelectionRow({ onUpdate }: SelectionRowProps) {
 
     function handleSelectedTimeChanged(value: string) {
         switch(value) {
+            case 'today':
+                let currentDate: String = new Date().toISOString().split('T')[0];
+                setStartDate(currentDate);
+                setEndDate(currentDate);
+                break;
+            case 'lastSevenDays':
+                let currentDate: String = new Date().toISOString().split('T')[0];
+                let sevenDaysAgoDate: String = (new Date().getDate() - 7).toISOString().split('T')[0];
+                break;
+            case 'thisMonth':
+                let [year, month]: [string, string] = new Date.toISOString().split('-').splice(0, 2);
+                setStartDate(`${year}-${month}-01`);
+                setEndDate(`${year}-${month}-30`);
+                break;
+            case 'thisYear':
+                let year: String = new Date().toISOString().split('-')[0];
+                setStartDate(`${year}-01-01`);
+                setEndDate(`${year}-12-31`);
+                break;
             default:
                 setTimestamp(value);
                 break;
@@ -34,9 +55,9 @@ export default function SelectionRow({ onUpdate }: SelectionRowProps) {
 
                 <select value={timestamp} onChange={e => handleSelectedTimeChanged(e.target.value)}>
                     <option value={'today'}>Today</option>
-                    <option value={'this_week'}>This Week</option>
-                    <option value={'this_month'}>This Month</option>
-                    <option value={'this_year'}>This Year</option>
+                    <option value={'lastSevenDays'}>Last 7 Days</option>
+                    <option value={'thisMonth'}>This Month</option>
+                    <option value={'thisYear'}>This Year</option>
                 </select>
             </div>
             <button onClick={() => clicked ? setClicked(false) : setClicked(true)}>↺</button>
