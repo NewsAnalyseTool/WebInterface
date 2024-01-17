e6d394b3bb38d50e3bb5c991b673982c2c2cd4f8
clzander <cle.zander@gmail.com>
2024-01-17 13:16:09 +0100

    adapted test backend to send back more realistic data

--- ui/test_backend/app.py
+++ ui/test_backend/app.py
@@ -30,6 +30,7 @@ class Source:
         self.neu_articles = sum(category.neu for category in self.categories)
         self.neg_articles = sum(category.neg for category in self.categories)
         self.article_count = self.pos_articles + self.neu_articles + self.neg_articles
+        self.article_perc = 0
         self.pos_articles_perc = round(((self.pos_articles / self.article_count) * 100), 2)
         self.neu_articles_perc = round(((self.neu_articles / self.article_count) * 100), 2)
         self.neg_articles_perc = round(((self.neg_articles / self.article_count) * 100), 2)
@@ -43,6 +44,10 @@ class Response:
 
         self.total_articles = self.red.article_count + self.nyt.article_count + self.ts.article_count
         self.total_categories = len(self.red.categories) + len(self.nyt.categories) + len(self.ts.categories)
+
+        self.red.article_perc = round(((self.red.article_count / self.total_articles) * 100), 2)
+        self.nyt.article_perc = round(((self.nyt.article_count / self.total_articles) * 100), 2)
+        self.ts.article_perc = round(((self.ts.article_count / self.total_articles) * 100), 2)
 
         self.red_perc = round(((self.red.article_count / self.total_articles) * 100), 2)
         self.nyt_perc = round(((self.nyt.article_count / self.total_articles) * 100), 2)
@@ -65,7 +70,7 @@ class TestDataEncoder(json.JSONEncoder):
             return {
                 'name': obj.name,
                 'articleCount': obj.article_count,
-                'articlePerc': 10,
+                'articlePerc': obj.article_perc,
                 'categoryCount': len(obj.categories),
                 'posArticles': obj.pos_articles,
                 'posArticlesPerc': obj.pos_articles_perc,
@@ -101,16 +106,23 @@ class Day:
         self.neg = random.randint(0, 15)
 
 class Timeline:
-    def __init__(self):
-        start_date = datetime(2023, 1, 1)
-        end_date = datetime(2023, 1, 31)
-        current_date = start_date
-        self.days = []
+    def __init__(self, start_date_str, end_date_str):
+        try:
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+            end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
 
-        while current_date <= end_date:
-            formatted_date = current_date.strftime('%Y-%m-%d')
-            self.days.append(Day(formatted_date))
-            current_date += timedelta(days=1)
+            current_date = start_date
+            self.days = []
+
+            while current_date <= end_date:
+                formatted_date = current_date.strftime('%Y-%m-%d')
+                self.days.append(Day(formatted_date))
+                current_date += timedelta(days=1)
+
+        except ValueError as e:
+            return f"Error parsing date: {str(e)}", 400
+
+        
 
 
 class TestTrendEncoder(json.JSONEncoder):
@@ -130,7 +142,10 @@ class TestTrendEncoder(json.JSONEncoder):
 
 @app.route("/api/trend", methods=["GET"])
 def get_trend():
-    return jsonify(TestTrendEncoder().default(Timeline()))
+    start_date_str = request.args.get('startDate')
+    end_date_str = request.args.get('endDate')
+
+    return jsonify(TestTrendEncoder().default(Timeline(start_date_str, end_date_str)))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
