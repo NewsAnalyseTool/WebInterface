932e494c9c9e8cb34fd2db2fd8926352beec5aa4
clzander <cle.zander@gmail.com>
2024-01-24 11:45:23 +0100

    sentiment chart now has a legend and tooltips, used material ui for visualizing chart therfore react-chart-js2 was removed as a dependency

--- ui/src/components/SentimentChart/SentimentChart.tsx
+++ ui/src/components/SentimentChart/SentimentChart.tsx
@@ -1,6 +1,4 @@
-import { Doughnut } from "react-chartjs-2";
-import {Chart, ChartOptions, ArcElement} from 'chart.js';
-Chart.register(ArcElement);
+import { PieChart } from "@mui/x-charts";
 
 interface SentimentChartProps {
     positives: number;
@@ -8,31 +6,43 @@ interface SentimentChartProps {
     negatives: number;
 }
 
-export default function SentimentChart({ positives, neutrals ,negatives }: SentimentChartProps) {
-    
-    const data = {
-        labels: ["Positive", "Neutral", "Negative"],
-        datasets: [
-            {
-                data: [positives, neutrals, negatives],
-                backgroundColor: ["#14AE5C", "#FFCD29", "#F24B22"],
-            },
-        ],
-    };
+export default function SentimentChart({ positives, neutrals, negatives }: SentimentChartProps) {
 
-    const options: ChartOptions<'doughnut'> = {
-        cutout: "60%",
-        rotation: -90,
-        circumference: 180,
-        plugins: {
-            legend: {
-                display: true,
-                position: "bottom",
-            },
-        },
-    };
+    const colors = ["#14AE5C", "#FFCD29", "#F24B22"]
 
     return (
-        <Doughnut data={data} options={options} />
+        //<Doughnut data={data} options={options} />
+
+        <PieChart
+            colors={colors}
+            series={[
+                {
+                    data: [
+                        { value: positives, label: "positive" },
+                        { value: neutrals, label: "neutral" },
+                        { value: negatives, label: "negative" },
+                    ],
+                    innerRadius: 65,
+                    startAngle: -90,
+                    endAngle: 90,
+                },
+                
+            ]}
+            slotProps={{
+                legend: {
+                  direction: 'row',
+                  position: { vertical: 'middle', horizontal: 'middle' },
+                  padding: 0,
+                },
+              }}
+            margin={{
+                bottom: 100,
+                top: 0,
+                left: 0,
+                right: 0
+            }}
+            width={400}
+            height={350}
+        />
     );
 }
